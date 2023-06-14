8e204ae4a7ccaa04a34617b7f47ad7a8b7630f34
Guitton Yann <yann.guitton@gmail.com>
2023-06-14 15:05:58 +0200

    Update bioconda.R back to basic only try xcms metaMS install

--- packages/bioconda.R
+++ packages/bioconda.R
@@ -2,30 +2,30 @@ options(repos=structure(c(CRAN="https://cran.rstudio.com/")))
 
 # bioconductor base
 if (!requireNamespace("BiocManager", quietly = TRUE))
-install.packages("BiocManager")
-BiocManager::install(version = "3.12", ask = FALSE)
+install.packages("BiocManager") # 3.11
+# MS data processing
+BiocManager::install("xcms", ask=FALSE) # 3.10.2
+#BiocManager::install("CAMERA", ask=FALSE)
+BiocManager::install("metaMS", ask=FALSE)
 
-# bioconductor packages
-BiocManager::install(c("edgeR", "Rgraphviz", "biomaRt", "topGO", "limma", "DESeq2", "cummeRbund", "Biostrings", "GenomicRanges", "Rsamtools", "Glimma", "Rgraphviz", "ComplexHeatmap", "goSTAG", "coseq", "mixOmics", "iClusterPlus", "MOFA2"))
+# Statistics
+#BiocManager::install("ropls")
+#BiocManager::install("mixOmics",, ask=FALSE)
+
+#Annotation
+# BiocManager::install("Spectra")
+# BiocManager::install("CompoundDb")
+# BiocManager::install("MetaboAnnotation")
+
 
 ## Installation instructions for the R for Mass Spectrometry tutorial
 ##      https://rformassspectrometry.github.io/docs/
 
 ## Install packages
-BiocManager::install("factoextra")
-BiocManager::install("msdata")
-BiocManager::install("mzR")
-BiocManager::install("MsCoreUtils")
-BiocManager::install("xcms")
-BiocManager::install("CAMERA")
-BiocManager::install("metaMS")
-BiocManager::install("Spectra")
-BiocManager::install("CompoundDb")
-BiocManager::install("MetaboAnnotation")
-BiocManager::install("MsExperiment")
-BiocManager::install("QFeatures")
-BiocManager::install("pheatmap")
-BiocManager::install("limma")
-BiocManager::install("MSnID")
-BiocManager::install("RforMassSpectrometry/SpectraVis")
-BiocManager::install("ropls")
+# BiocManager::install("factoextra")
+# BiocManager::install("msdata")
+# BiocManager::install("MsExperiment")
+# BiocManager::install("QFeatures")
+# BiocManager::install("pheatmap")
+# BiocManager::install("MSnID")
+# BiocManager::install("gc-meox-tms")
